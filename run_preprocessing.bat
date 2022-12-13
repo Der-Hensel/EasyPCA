ca6213249510594cde812fc92204ed7f34bef467
Der-Hensel <109506200+Der-Hensel@users.noreply.github.com>
2022-12-13 12:06:12 +0100

    Update run_preprocessing.bat

--- run_preprocessing.bat
+++ run_preprocessing.bat
@@ -1,7 +1,7 @@
 ECHO ON
 set root=C:\Users\%username%\anaconda3\
 call %root%\Scripts\activate.bat %root%
-
+start ""  http://127.0.0.1:8080/
 call conda activate ezPCA
 
 call python ezPCA_preprocessing.py
